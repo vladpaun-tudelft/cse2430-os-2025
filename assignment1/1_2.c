a5f52548b3b338e65390ed1a11c65c2653d59a02
vladpaun1 <vlad.paun06@gmail.com>
2026-02-15 19:09:51 +0100

    finish 1_2

--- assignment1/1_2.c
+++ assignment1/1_2.c
@@ -1,5 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <dirent.h>
+
 int main(void) {
-    
-    
+    system("mkdir -p tmpdir_system");
+    system("ls");
+
+    mkdir("tmpdir_lib", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+    DIR *d = opendir(".");
+    struct dirent *entry = readdir(d);
+    while (entry != NULL) {
+        printf("%s\n", entry->d_name);
+        entry = readdir(d);
+    }
+
+    closedir(d);
+
     return 0;
 }
