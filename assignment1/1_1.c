e2210a2bcaa17b35f1d19a8ea0116d502ef4836c
vladpaun1 <vlad.paun06@gmail.com>
2026-02-15 18:38:31 +0100

    start assignment1

---
+++ assignment1/1_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(void) {
+    char *name = "Vlad Paun";
+    int student_number = 6152937;
+
+    printf("%s %i\n",name,student_number);
+
+    pid_t pid = getpid();
+    printf("%ld\n",(long int)pid);
+
+    return 0;
+}
